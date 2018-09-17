af9f61a2b4eb657b03e6d7b6865ea78863cec7eb
henriquesan14 <33522361+henriquesan14@users.noreply.github.com>
2018-09-16 22:34:48 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -9,7 +8,7 @@ import org.jdom2.input.SAXBuilder;
 
 public class Principal {
 	public static void main(String[] args) {
-		File f=new File("C:/Users/ricog/Desktop/PIBMunicipiosPE.xml"); //Substitui pelo o diretorio que o arquivo está
+		File f=new File("C:/Users/ricog/Desktop/PIBMunicipiosPE.xml"); //Substitua pelo o diretorio que o arquivo está
 		
 		SAXBuilder sb=new SAXBuilder();
 		
@@ -29,8 +28,8 @@ public class Principal {
 		List<Element> registros=root.getChild("corpo").getChild("registros").getChildren(); //captura os registros
 		
 		System.out.println("--"+titulo+"--");
-		for(Element element: registros){  //for pra iterar os registros
-			System.out.println(element.getAttributeValue("campo1")+" - "+element.getAttributeValue("campo2"));
+		for(Element registro: registros){  //for pra iterar os registros
+			System.out.println(registro.getAttributeValue("campo1")+" - "+registro.getAttributeValue("campo2"));
 		}
 		
 	}
